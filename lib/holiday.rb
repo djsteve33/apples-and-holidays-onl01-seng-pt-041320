f384ecdca1a37fab68ca76ad6472b09dae05abeb
Steve Womble <swomble@aol.com>
2020-04-25 23:17:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,7 +56,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.each do |season, holidays|
+  holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize!}:"
     holidays.keys.each do |holiday|
       count = 0
